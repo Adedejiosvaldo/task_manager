6bbb20d38cc9776cbb1e0a877e3ded18e5572d4f
Adedejiosvaldo <adedejiosvaldo@gmail.com>
2023-08-09 09:49:44 +0100

    Feature-Added deleteTask controller

--- controllers/Tasks.js
+++ controllers/Tasks.js
@@ -3,6 +3,10 @@ const Tasks = require("../models/Tasks");
 const getAllTasks = async (req, res) => {
   try {
     const task = await Tasks.find({});
+
+    if (task.length === 0) {
+      return res.status(404).json({ message: `No Tasks At the Moment` });
+    }
     res.status(200).json({ task });
   } catch (error) {
     res.status(500).json({ message: error });
@@ -37,8 +41,18 @@ const updateTask = (req, res) => {
   res.send("Task has been updated");
 };
 
-const deleteTask = (req, res) => {
-  res.send("Successfully Deleted");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+
+    const task = await Tasks.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: "Task DOesnt exist" });
+    }
+    res.status(200).json({ message: "Sucessfully Deleted " });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 module.exports = { getAllTasks, getTask, createTask, updateTask, deleteTask };
